937f9e75892d8d8130319090366add10ccd450b6
triplekkoh <triplekkoh@gmail.com>
2017-11-24 14:25:20 +0800

    Version 1 without best fit
    Still working on best fit

---
+++ PartitionProblem/PartitionProblem/Main.cpp
@@ -0,0 +1,160 @@
+#include <math.h>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h> 
+#include <cstdlib>
+
+
+using namespace std;
+
+int SetSize=10; //Size of element
+int Set[10] = {}; //Size of element
+//int *Set;
+int FirstSet[10] = {}; //Size of element
+int SecondSet[10] = {}; //Size of element
+
+int max = 50; //maximum number
+int min = 1; //minimum number
+
+int generate_random_set_yes(); //random generate number with garanteed solution
+int generate_random_set(); //random generate number without garanteed solution
+bool dynamic_programming(int set[], int n, int sum);
+//int best_fit();
+
+
+int main()
+{
+	//while (generate_random_set_yes() == 99)//eliminate possibility of having sum that is odd
+	//{
+	//	generate_random_set_yes();
+	//}
+
+	//cout << "Enter the size of set: " << endl;
+	//cin >> SetSize;
+	//Set = new int[SetSize];	
+	//
+	//generate_random_set_yes();
+	//best_fit();
+	//delete[]  Set;
+
+	generate_random_set_yes();
+	best_fit();
+	return 0;
+}
+
+int generate_random_set_yes()
+{
+	int total = 0;
+	srand(time(NULL));
+	cout << endl;
+	for (int i = 0; i < SetSize - 1; i++) //reserve 1 number for second set
+	{
+
+		int result = rand() % (max - min + 1) + min; //generate random between max and min
+		cout << result << endl;
+		Set[i] = result;
+		total = total + result;
+	}
+	if (total % 2 == 1)
+	{
+		return 99;//eliminate possibility of having sum that is odd		
+	}
+	int half = total / 2;
+	int arrcounter = 0;
+	int firstsum = 0, secondsum = 0;
+
+	//one by one insert an element into the first array until the sum exceed the half of total sum
+	while (half > 0)
+	{
+		FirstSet[arrcounter] = Set[arrcounter];
+		half = half - Set[arrcounter];
+		firstsum = firstsum + Set[arrcounter];
+		arrcounter++;
+	}
+	cout << "the first sum is " << firstsum<<endl;
+	//insert the remaining into the second array and calculate the sum
+	int secondcounter = 0;
+	while (arrcounter < SetSize-1)
+	{
+		SecondSet[secondcounter] = Set[arrcounter];
+		secondsum = secondsum + Set[arrcounter];
+		arrcounter++;
+		secondcounter++;
+	}
+	cout << "the second sum is " << secondsum<<endl<<"diff"<< firstsum - secondsum;
+	//calculate the difference between sum of first array and second array
+	//put the diffenrence into the second array
+
+	if (arrcounter == SetSize)
+	{		
+		SecondSet[secondcounter] = firstsum - secondsum;
+	}
+
+	for (int i = 0; i < SetSize; i++)
+	{
+		if (FirstSet[i] != 0)
+		{
+			Set[i] = FirstSet[i];
+		}
+		else
+		{
+			for (int j = 0; j < SetSize; j++)
+			{
+				if (SecondSet[j] == 0)
+				{
+					break;
+				}
+				Set[i] = SecondSet[j];
+				i++;
+			}			
+		}
+	}
+	Set[SetSize-1] = firstsum - secondsum;
+	cout << "The element in set are" << endl;
+	for (int j = 0; j < SetSize; j++)
+	{
+		cout << Set[j] << endl;
+	}
+
+	return 0;
+}
+
+int generate_random_set()
+{
+	int total = 0;
+	srand(time(NULL));
+	cout << "not guarantee solution" << endl;
+	for (int i = 0; i < SetSize; i++)
+	{
+
+		int result = rand() % (max - min + 1) + min; //generate random between max and min
+		cout << result << endl;
+		Set[i] = result;
+		total = total + result;
+	}
+	if (total % 2 == 1)//eliminate possibility of having sum that is odd
+	{
+		return 99;
+	}
+	return 0;
+}
+
+bool dynamic_programming(int set[], int n, int sum)
+{
+	// Base Cases
+	if (sum == 0)
+		return true;
+	if (n == 0 && sum != 0)
+		return false;
+
+	// If last element is greater than sum, then ignore it
+	if (set[n - 1] > sum)
+		return dynamic_programming(set, n - 1, sum);
+
+	/* else, check if sum can be obtained by any of the following
+	(a) including the last element
+	(b) excluding the last element   */
+	return dynamic_programming(set, n - 1, sum) || dynamic_programming(set, n - 1, sum - set[n - 1]);
+
+	//http://www.geeksforgeeks.org/dynamic-programming-subset-sum-problem/
+}
